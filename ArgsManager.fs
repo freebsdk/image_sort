5ae450e277d27378e00b9c192ca2a50462e22d6b
SeokYoung Shin <aceorb@gmail.com>
2020-12-13 23:36:45 +0900

    Cleanup code.

--- ArgsManager.fs
+++ ArgsManager.fs
@@ -34,8 +34,8 @@ type ArgsManager () =
         
     let getArgValue (argv : string[]) (argIdx : int) =
         let arg = argv.[argIdx]
-        if arg.StartsWith("\"") then
-            // strip double quotes
+        if arg.StartsWith("\"") && arg.EndsWith("\"") then
+            // Remove the double quotation marks at the front and back.
             arg.Substring(1, arg.Length-2)
         else
             arg
@@ -53,11 +53,13 @@ type ArgsManager () =
         for elem in parseElems do
             match findArgIdx <| argv <| elem.Key <| 0 with
             | Some argIdx ->
+                // Remove --
+                let pureKey = elem.Key.Substring(2,elem.Key.Length-2)
                 if elem.NeedVal then
                     let value = getArgValue <| argv <| argIdx+1
-                    dic.Add(elem.Key, value)
+                    dic.Add(pureKey, value)
                 else
-                    dic.Add(elem.Key, "")
+                    dic.Add(pureKey, "")
             | None ->
                 if elem.ArgsType = ARGS_TYPE_REQUIRED then
                     failwith "Cannot found the required argument."
